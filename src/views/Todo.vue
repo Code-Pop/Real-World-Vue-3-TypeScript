0109fb7a8045dfdcc4013edeb2537631924cac91
Ben Hong <ben@bencodezen.io>
2021-03-02 21:11:48 -0500

    feature: add computed property for todo

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -8,8 +8,24 @@ export default defineComponent({
       label: '',
       type: 'personal'
     } as TodoItem,
-    taskItems: [] as TodoItem[]
+    taskItems: [] as TodoItem[],
+    listFilter: 'all'
   }),
+  computed: {
+    filteredTasks(): TodoItem[] {
+      if (this.listFilter === 'complete') {
+        return this.taskItems.filter(
+          (item: TodoItem) => item.isComplete === true
+        )
+      } else if (this.listFilter === 'incomplete') {
+        return this.taskItems.filter(
+          (item: TodoItem) => item.isComplete === false
+        )
+      } else {
+        return this.taskItems
+      }
+    }
+  },
   methods: {
     addTask() {
       this.taskItems.push({
@@ -22,31 +38,49 @@ export default defineComponent({
 </script>
 
 <template>
-  <h1>Todo</h1>
-  <form @submit.prevent>
-    <div class="mb1">
-      <label for="task-label">Task Label: </label>
-      <input type="text" id="task-label" v-model="newTask.label" />
-    </div>
+  <div class="todo-page">
+    <h1>Todo</h1>
+    <form @submit.prevent>
+      <div class="mb1">
+        <label for="task-label">Task Label: </label>
+        <input type="text" id="task-label" v-model="newTask.label" />
+      </div>
+      <div class="mb1">
+        <label for="task-type">Take Type: </label>
+        <select name="task-type" id="task-type" v-model="newTask.type">
+          <option value="personal">Personal</option>
+          <option value="work">Work</option>
+          <option value="miscellaneous">Miscellaneous</option>
+        </select>
+      </div>
+      <button @click="addTask">Add Task</button>
+    </form>
+    <h2>Task List</h2>
     <div>
-      <label for="task-type">Take Type: </label>
-      <select name="task-type" id="task-type" v-model="newTask.type">
-        <option value="personal">Personal</option>
-        <option value="work">Work</option>
-        <option value="miscellaneous">Miscellaneous</option>
-      </select>
+      <button @click="listFilter = 'all'">All</button> |
+      <button @click="listFilter = 'incomplete'">Incomplete</button> |
+      <button @click="listFilter = 'complete'">Complete</button>
     </div>
-    <button @click="addTask">Add Task</button>
-  </form>
-  <h2>Task List</h2>
-  <ul>
-    <li v-for="(task, index) in taskItems" :key="`task-${index}`">
-      {{ task }}
-    </li>
-  </ul>
+    <ul>
+      <li v-for="(task, index) in filteredTasks" :key="`task-${index}`">
+        <input type="checkbox" v-model="task.isComplete" /> {{ task.label }} ({{
+          task.type
+        }})
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
+.todo-page {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+ul {
+  list-style: none;
+}
+
 .mb1 {
   margin-bottom: 1rem;
 }
